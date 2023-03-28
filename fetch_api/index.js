d3cfd524719739cafb1aaaa549e18646e942920e
AidanGood <aidangood5@gmail.com>
2023-03-28 11:12:28 -0400

    Completed part 2

--- fetch_api/index.js
+++ fetch_api/index.js
@@ -13,6 +13,39 @@ function getNewDog() {
 
 }
 
+function getCatFact() {
+    var url = "https://cat-fact.herokuapp.com/facts/random"
+
+    console.log("making fetch to", url)
+
+    fetch(url)
+        .then(resp=>{return resp.json()})
+        .then(json=>{
+            console.log(json)
+
+            document.getElementById("catFact").innerHTML = json.text
+    })
+
+}
+
+
+function getPoem() {
+    var url = "https://poetrydb.org/random"
+
+    console.log("making fetch to", url)
+
+    fetch(url)
+        .then(resp=>{return resp.json()})
+        .then(json=>{
+            console.log(json[0])
+
+            document.getElementById("author").innerHTML = json[0].author
+            document.getElementById("lines").innerHTML = json[0].lines.join("<br>");
+
+
+    })
+
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Hello World!");
